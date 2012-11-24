5097c1294af418a44aa39385d6fe88ae07e04b93
Fabian <fabian@arctium.org>
2012-11-24 16:07:11 +0100

    Some improvements to the packetlogging class.

--- Framework/Logging/PacketLogging/PacketLog.cs
+++ Framework/Logging/PacketLogging/PacketLog.cs
@@ -20,132 +20,144 @@ using Framework.Network.Packets;
 using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Framework.Logging.PacketLogging
 {
     public class PacketLog
     {
+        static TextWriter logWriter;
+        static Object syncObj = new Object();
+
         public static void WritePacket(string clientInfo, PacketWriter serverPacket = null, PacketReader clientPacket = null)
         {
-            using (StreamWriter logWriter = File.AppendText("Packet.log"))
+            lock (syncObj)
             {
-                if (serverPacket != null)
+                try
                 {
-                    logWriter.WriteLine(String.Format("Client: {0}", clientInfo));
+                    StringBuilder sb = new StringBuilder();
 
-                    if (Enum.IsDefined(typeof(LegacyMessage), serverPacket.Opcode))
+                    if (serverPacket != null)
                     {
-                        logWriter.WriteLine("Type: LegacyMessage");
-                        logWriter.WriteLine(String.Format("Name: {0}", Enum.GetName(typeof(LegacyMessage), serverPacket.Opcode)));
-                    }
-                    else if (Enum.IsDefined(typeof(JAMCMessage), serverPacket.Opcode))
-                    {
-                        logWriter.WriteLine("Type: JAMCMessage");
-                        logWriter.WriteLine(String.Format("Name: {0}", Enum.GetName(typeof(JAMCMessage), serverPacket.Opcode)));
-                    }
-                    else if (Enum.IsDefined(typeof(Message), serverPacket.Opcode))
-                    {
-                        logWriter.WriteLine("Type: Message");
-                        logWriter.WriteLine(String.Format("Name: {0}", Enum.GetName(typeof(Message), serverPacket.Opcode)));
-                    }
-                    else
-                    {
-                        logWriter.WriteLine("Type: JAMCCMessage");
-                        logWriter.WriteLine(String.Format("Name: {0}", Enum.GetName(typeof(JAMCCMessage), serverPacket.Opcode)));
-                    }
+                        sb.AppendLine(String.Format("Client: {0}", clientInfo));
 
-                    logWriter.WriteLine(String.Format("Value: 0x{0:X} ({1})", serverPacket.Opcode, serverPacket.Opcode));
-                    logWriter.WriteLine(String.Format("Length: {0}", serverPacket.Size - 2));
-
-                    logWriter.WriteLine("|----------------------------------------------------------------|");
-                    logWriter.WriteLine("| 00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F |");
-                    logWriter.WriteLine("|----------------------------------------------------------------|");
-                    logWriter.Write("|");
-
-                    if (serverPacket.Size - 2 != 0)
-                    {
-                        var data = serverPacket.ReadDataToSend().ToList();
-                        data.RemoveRange(0, 4);
-
-                        byte count = 0;
-                        data.ForEach(b =>
+                        if (Enum.IsDefined(typeof(LegacyMessage), serverPacket.Opcode))
                         {
-                            if (b <= 0xF)
-                                logWriter.Write(String.Format(" 0{0:X} ", b));
-                            else
-                                logWriter.Write(String.Format(" {0:X} ", b));
+                            sb.AppendLine("Type: LegacyMessage");
+                            sb.AppendLine(String.Format("Name: {0}", Enum.GetName(typeof(LegacyMessage), serverPacket.Opcode)));
+                        }
+                        else if (Enum.IsDefined(typeof(JAMCMessage), serverPacket.Opcode))
+                        {
+                            sb.AppendLine("Type: JAMCMessage");
+                            sb.AppendLine(String.Format("Name: {0}", Enum.GetName(typeof(JAMCMessage), serverPacket.Opcode)));
+                        }
+                        else if (Enum.IsDefined(typeof(Message), serverPacket.Opcode))
+                        {
+                            sb.AppendLine("Type: Message");
+                            sb.AppendLine(String.Format("Name: {0}", Enum.GetName(typeof(Message), serverPacket.Opcode)));
+                        }
+                        else
+                        {
+                            sb.AppendLine("Type: JAMCCMessage");
+                            sb.AppendLine(String.Format("Name: {0}", Enum.GetName(typeof(JAMCCMessage), serverPacket.Opcode)));
+                        }
 
-                            if (count == 15)
+                        sb.AppendLine(String.Format("Value: 0x{0:X} ({1})", serverPacket.Opcode, serverPacket.Opcode));
+                        sb.AppendLine(String.Format("Length: {0}", serverPacket.Size - 2));
+
+                        sb.AppendLine("|----------------------------------------------------------------|");
+                        sb.AppendLine("| 00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F |");
+                        sb.AppendLine("|----------------------------------------------------------------|");
+                        sb.Append("|");
+
+                        if (serverPacket.Size - 2 != 0)
+                        {
+                            var data = serverPacket.ReadDataToSend().ToList();
+                            data.RemoveRange(0, 4);
+
+                            byte count = 0;
+                            data.ForEach(b =>
                             {
-                                logWriter.Write("|");
-                                logWriter.WriteLine();
-                                logWriter.Write("|");
-                                count = 0;
-                            }
-                            else
-                                count++;
-                        });
+                                if (b <= 0xF)
+                                    sb.Append(String.Format(" 0{0:X} ", b));
+                                else
+                                    sb.Append(String.Format(" {0:X} ", b));
 
-                        logWriter.WriteLine("");
-                        logWriter.WriteLine("|----------------------------------------------------------------|");
+                                if (count == 15)
+                                {
+                                    sb.Append("|");
+                                    sb.AppendLine();
+                                    sb.Append("|");
+                                    count = 0;
+                                }
+                                else
+                                    count++;
+                            });
+
+                            sb.AppendLine("");
+                            sb.AppendLine("|----------------------------------------------------------------|");
+                        }
+
+                        sb.AppendLine("");
+                        sb.AppendLine("");
                     }
 
-                    logWriter.WriteLine("");
-                    logWriter.WriteLine("");
+                    if (clientPacket != null)
+                    {
+                        sb.AppendLine(String.Format("Client: {0}", clientInfo));
+                        sb.AppendLine("Type: ClientMessage");
 
+                        if (Enum.IsDefined(typeof(ClientMessage), clientPacket.Opcode))
+                            sb.AppendLine(String.Format("Name: {0}", clientPacket.Opcode));
+                        else
+                            sb.AppendLine(String.Format("Name: {0}", "Unknown"));
 
-                    logWriter.Close();
+                        sb.AppendLine(String.Format("Value: 0x{0:X} ({1})", (ushort)clientPacket.Opcode, (ushort)clientPacket.Opcode));
+                        sb.AppendLine(String.Format("Length: {0}", clientPacket.Size));
+
+                        sb.AppendLine("|----------------------------------------------------------------|");
+                        sb.AppendLine("| 00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F |");
+                        sb.AppendLine("|----------------------------------------------------------------|");
+                        sb.Append("|");
+
+                        if (clientPacket.Size - 2 != 0)
+                        {
+                            var data = clientPacket.Storage.ToList();
+
+                            byte count = 0;
+                            data.ForEach(b =>
+                            {
+
+                                if (b <= 0xF)
+                                    sb.Append(String.Format(" 0{0:X} ", b));
+                                else
+                                    sb.Append(String.Format(" {0:X} ", b));
+
+                                if (count == 15)
+                                {
+                                    sb.Append("|");
+                                    sb.AppendLine();
+                                    sb.Append("|");
+                                    count = 0;
+                                }
+                                else
+                                    count++;
+                            });
+
+                            sb.AppendLine();
+                            sb.Append("|----------------------------------------------------------------|");
+                        }
+
+                        sb.AppendLine("");
+                        sb.AppendLine("");
+                    }
+
+                    logWriter = TextWriter.Synchronized(File.AppendText("Packet.log"));
+                    logWriter.WriteLine(sb.ToString());
+                    logWriter.Flush();
                 }
-
-                if (clientPacket != null)
+                finally
                 {
-                    logWriter.WriteLine(String.Format("Client: {0}", clientInfo));
-                    logWriter.WriteLine("Type: ClientMessage");
-
-                    if (Enum.IsDefined(typeof(ClientMessage), clientPacket.Opcode))
-                        logWriter.WriteLine(String.Format("Name: {0}", clientPacket.Opcode));
-                    else
-                        logWriter.WriteLine(String.Format("Name: {0}", "Unknown"));
-
-                    logWriter.WriteLine(String.Format("Value: 0x{0:X} ({1})", (ushort)clientPacket.Opcode, (ushort)clientPacket.Opcode));
-                    logWriter.WriteLine(String.Format("Length: {0}", clientPacket.Size));
-
-                    logWriter.WriteLine("|----------------------------------------------------------------|");
-                    logWriter.WriteLine("| 00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F |");
-                    logWriter.WriteLine("|----------------------------------------------------------------|");
-                    logWriter.Write("|");
-
-                    if (clientPacket.Size - 2 != 0)
-                    {
-                        var data = clientPacket.Storage.ToList();
-
-                        byte count = 0;
-                        data.ForEach(b =>
-                        {
-
-                            if (b <= 0xF)
-                                logWriter.Write(String.Format(" 0{0:X} ", b));
-                            else
-                                logWriter.Write(String.Format(" {0:X} ", b));
-
-                            if (count == 15)
-                            {
-                                logWriter.Write("|");
-                                logWriter.WriteLine();
-                                logWriter.Write("|");
-                                count = 0;
-                            }
-                            else
-                                count++;
-                        });
-
-                        logWriter.WriteLine();
-                        logWriter.Write("|----------------------------------------------------------------|");
-                    }
-
-                    logWriter.WriteLine("");
-                    logWriter.WriteLine("");
-
                     logWriter.Close();
                 }
             }
